2ac8cc01db0e0b6603d58c5f5841b778db774d55
Rhys Bilson <rhysbilson@Rhyss-MacBook-Pro.local>
2026-07-24 16:45:49 +0100

    Enabled reading from datasheet

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
-print("Hello, World!")
+import pandas as pd
 
+# Read the Excel file
+employees = pd.read_excel("Employee Datasheet.xlsx", header=1)
+
+# Count total workers
+total_workers = (len(employees))
+
+print(f"Total workers: {total_workers}")
+
+print(employees.columns)
+
+# Count available workers
+available_workers = len(employees[employees["Available"] == "O"])
+
+print(f"Available workers: {available_workers}")
+
+# Print the first employee and all their details
+print("\nFirst employee:")
+print(employees.iloc[0])
